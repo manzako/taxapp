a2c000a23e69e2082026e545329feac2930229cd
Jonathan Manzako <jonathanmanzako@gmail.com>
2020-11-10 00:40:17 +0100

    ajout user

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
-  const fiche = sequelize.define(
+  const user = sequelize.define(
     "user",
     {
       nom: {
