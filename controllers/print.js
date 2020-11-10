3ee87e2d19aa91e8daa034edfe6445a266c7ddac
unknown <jonathanmanzako@gmail.com>
2020-11-10 16:58:44 +0100

    generation printer dynamic

--- controllers/print.js
+++ controllers/print.js
@@ -1,4 +1,4 @@
-//const db = require("../../models");
+const db = require("../models");
 
 
 module.exports.controller = (app) => {
@@ -6,6 +6,14 @@ module.exports.controller = (app) => {
   app.get('/print',function (req, res) {
     res.render('pages/print');
   });
+  app.get('/print/fiches/:id',async function (req, res) {
+
+    const fiche=await db.fiche.findAll({where:{id:req.params.id}});
+    res.render('pages/print',{
+        fiche:fiche
+    })
+  });
+
 
 
   //DEBUG=express_app:* npm start
